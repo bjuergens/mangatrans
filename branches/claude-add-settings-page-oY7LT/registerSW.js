a5dc65b9684705f7fb403eb82d66272161003446
bjuergens <bjuergens@users.noreply.github.com>
2026-03-07 14:27:53 +0000

    Deploying to gh-pages from @ bjuergens/mangatrans@5a7ba247ccda99200590bf12a10c10e33068235b 🚀

---
+++ branches/claude-add-settings-page-oY7LT/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mangatrans/branches/claude-add-settings-page-oY7LT/sw.js', { scope: '/mangatrans/branches/claude-add-settings-page-oY7LT/' })})}
