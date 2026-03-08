c733675f3a480877835daae53637107f0c65faf8
bjuergens <bjuergens@users.noreply.github.com>
2026-03-08 13:31:56 +0000

    Deploying to gh-pages from @ bjuergens/mangatrans@d5352f0146015f24838444bbdea1197109e1f7fb 🚀

---
+++ branches/claude-fix-textbox-overlay-pENDa/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mangatrans/branches/claude-fix-textbox-overlay-pENDa/sw.js', { scope: '/mangatrans/branches/claude-fix-textbox-overlay-pENDa/' })})}
