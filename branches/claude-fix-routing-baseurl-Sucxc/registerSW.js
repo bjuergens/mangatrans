75043d7e642cfdfa8325746b2371898e67364ded
bjuergens <bjuergens@users.noreply.github.com>
2026-03-08 07:54:05 +0000

    Deploying to gh-pages from @ bjuergens/mangatrans@a9cfc755932306a2c0f34b331d69b1233af28147 🚀

---
+++ branches/claude-fix-routing-baseurl-Sucxc/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mangatrans/branches/claude-fix-routing-baseurl-Sucxc/sw.js', { scope: '/mangatrans/branches/claude-fix-routing-baseurl-Sucxc/' })})}
