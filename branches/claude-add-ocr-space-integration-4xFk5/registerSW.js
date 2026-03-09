d9ed600dbbcb5b117f7cf9e24e6f60a9cc268a0f
bjuergens <bjuergens@users.noreply.github.com>
2026-03-09 07:13:44 +0000

    Deploying to gh-pages from @ bjuergens/mangatrans@bfecdc438053514ab4564fc749353911e43dd7a1 🚀

---
+++ branches/claude-add-ocr-space-integration-4xFk5/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mangatrans/branches/claude-add-ocr-space-integration-4xFk5/sw.js', { scope: '/mangatrans/branches/claude-add-ocr-space-integration-4xFk5/' })})}
